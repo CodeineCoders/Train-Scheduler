b131ebf7430a537523b17cfc93fdc2d983214abd
Brian Racek <rac3k@aol.com>
2018-06-25 19:54:28 -0400

    firebase api created

---
+++ assets/js/trainScheduler.js
@@ -0,0 +1,27 @@
+$(document).ready( function() {
+
+
+
+
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBwUgolHJtNB8h7yLC9Z8uCllmMQmMjuAA",
+    authDomain: "train-scheduler-743f3.firebaseapp.com",
+    databaseURL: "https://train-scheduler-743f3.firebaseio.com",
+    projectId: "train-scheduler-743f3",
+    storageBucket: "train-scheduler-743f3.appspot.com",
+    messagingSenderId: "373305274421"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+
+
+    
+
+
+
+});
